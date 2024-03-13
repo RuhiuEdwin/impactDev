f890dc87784a8415177a6359785d5a773e1e224e
Ruhiu Edwin <edwin.nganga@roamtech.com>
2024-03-13 13:11:54 +0300

    Persisting data on the step form

--- src/components/questionsComponent.tsx
+++ src/components/questionsComponent.tsx
@@ -10,26 +10,44 @@ import Five from "./questions/five";
 
 type closeWaitlistForm = () => void;
 
+type FormData = {
+  fullName: string;
+  emailAddress: string;
+  companyName: string;
+  helpType: string[];
+};
+
 const questionsComponent = ({
   closeWaitlistForm,
 }: {
   closeWaitlistForm: closeWaitlistForm;
 }) => {
   const [currentStep, setCurrentStep] = useState(1); // Initial step
+  const [formData, setFormData] = useState<FormData>({
+    fullName: "",
+    emailAddress: "",
+    companyName: "",
+    helpType: [],
+  });
 
   const handleNextStep = () => {
+    console.log(formData);
     setCurrentStep(currentStep + 1);
   };
   const handlePreviousStep = () => {
     setCurrentStep(currentStep - 1);
   };
 
+  const updateFormData = (data: Partial<FormData>) => {
+    setFormData((prevData) => ({ ...prevData, ...data }));
+  };
+
   return (
     <div
-      onClick={closeWaitlistForm}
+      // onClick={closeWaitlistForm}
       className="bg-navy bg-opacity-40 flex items-center justify-center w-screen h-screen absolute top-0 left-0 z-20"
     >
-      <div className="bg-olive rounded-sm lg:rounded-lg drop-shadow-xl flex flex-col items-center justify-center text-white lg:w-1/3 w-11/12 p-5 py-10 pb-10">
+      <div className="bg-olive rounded-sm lg:rounded-lg drop-shadow-xl flex flex-col items-center justify-center text-white lg:w-1/3 md:w-2/3 w-11/12 p-5 py-10 pb-10">
         {currentStep !== 6 ? (
           <h1 className="text-xl lg:text-3xl font-bold text-center pb-2 border-b border-b-darkGreen mb-5">
             Access Impact Tool-Box
@@ -39,7 +57,13 @@ const questionsComponent = ({
             Thank you for your responses
           </h1>
         )}
-        {currentStep === 1 && <Main onNextStep={handleNextStep} />}
+        {currentStep === 1 && (
+          <Main
+            onNextStep={handleNextStep}
+            formData={formData}
+            updateFormData={updateFormData}
+          />
+        )}
         {currentStep === 2 && (
           <One
             onNextStep={handleNextStep}
--- src/components/waitlistForm.tsx
+++ src/components/waitlistForm.tsx
@@ -1,13 +1,63 @@
+import React, { useState, useEffect } from "react";
+
 type FormData = {
   fullName: string;
   emailAddress: string;
   companyName: string;
-  helpType: string;
+  helpType: string[];
 };
 
-// Define the type of closeWaitlistForm
-type onNextStep = () => void;
-const waitlistForm = ({ onNextStep }: { onNextStep: onNextStep }) => {
+type WaitlistFormProps = {
+  onNextStep: () => void;
+  formData: FormData;
+  updateFormData: (data: Partial<FormData>) => void;
+};
+
+const waitlistForm: React.FC<WaitlistFormProps> = ({
+  onNextStep,
+  formData,
+  updateFormData,
+}) => {
+  const [isFormFilled, setIsFormFilled] = useState(false);
+
+  // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const { name, value, type } = e.target;
+  //   updateFormData({ [name]: value });
+  // };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type, checked } = e.target;
+
+    if (type === "checkbox") {
+      let updatedHelpType = [...formData.helpType];
+
+      if (checked) {
+        updatedHelpType.push(value); // Add to array if checked
+      } else {
+        updatedHelpType = updatedHelpType.filter((type) => type !== value); // Remove from array if unchecked
+      }
+
+      updateFormData({ [name]: updatedHelpType });
+    } else {
+      updateFormData({ [name]: value });
+    }
+  };
+
+
+  useEffect(() => {
+    // Check if all form fields are filled
+    const checkFormFilled = () => {
+      const { fullName, emailAddress, companyName, helpType } = formData;
+      return (
+        fullName !== "" &&
+        emailAddress !== "" &&
+        companyName !== "" &&
+        helpType.length > 0
+      );
+    };
+
+    setIsFormFilled(checkFormFilled());
+  }, [formData]);
+
   return (
     <div>
       <p className="text-sm lg:text-base  font-light text-center mt-2">
@@ -16,38 +66,87 @@ const waitlistForm = ({ onNextStep }: { onNextStep: onNextStep }) => {
           Impact resources & Toolkits 
         </span>
       </p>
-      <form className="flex flex-col gap-3 mt-5 items-start w-full">
+      <form className="flex flex-col gap-3 mt-5 items-start w-full ">
         <div className="flex flex-col gap-1 items-start w-full">
           <h4 className="font-light text-sm">FULL NAMES</h4>
-          <input className="w-full bg-white rounded-sm lg:p-3 pl-2 p-1" />
+          <input
+            type="text"
+            name="fullName"
+            value={formData.fullName}
+            onChange={handleChange}
+            placeholder="Full Name"
+            className="w-full bg-white rounded-sm lg:p-3 pl-2 p-1 text-navy"
+          />
         </div>
         <div className="flex flex-col gap-1 items-start w-full">
           <h4 className="font-light text-sm">EMAIL ADDRESS</h4>
-          <input className="w-full bg-white rounded-sm lg:p-3 pl-2 p-1" />
+          <input
+            type="email"
+            name="emailAddress"
+            value={formData.emailAddress}
+            onChange={handleChange}
+            placeholder="Email Address"
+            className="w-full bg-white rounded-sm lg:p-3 pl-2 p-1 text-navy"
+          />
         </div>
         <div className="flex flex-col gap-1 items-start w-full">
           <h4 className="font-light text-sm">COMPANY NAME</h4>
-          <input className="w-full bg-white rounded-sm lg:p-3 pl-2 p-1" />
+          <input
+            type="text"
+            name="companyName"
+            value={formData.companyName}
+            onChange={handleChange}
+            placeholder="Company Name"
+            className="w-full bg-white rounded-sm lg:p-3 pl-2 p-1 text-navy"
+          />
         </div>
         <div className="flex flex-col gap-1 items-start">
           <h4 className="font-light text-sm">HOW CAN WE HELP YOU?</h4>
           <div className="flex flex-col items-start">
             <div className="flex gap-1 items-center">
-              <input type="radio" id="" name="" value="" />
+              <input
+                type="checkbox"
+                id="guides"
+                name="helpType"
+                value="guides"
+                checked={formData.helpType.includes("guides")}
+                onChange={handleChange}
+              />
               <label className="text-xs lg:text-sm font-thin">GUIDES</label>
             </div>
             <div className="flex gap-1 items-center">
-              <input type="radio" id="" name="" value="" />
+              <input
+                type="checkbox"
+                id="toolkits"
+                name="helpType"
+                value="toolkits"
+                onChange={handleChange}
+                checked={formData.helpType.includes("toolkits")}
+              />
               <label className="text-xs lg:text-sm font-thin">TOOLKITS</label>
             </div>
             <div className="flex gap-1 items-center">
-              <input type="radio" id="" name="" value="" />
+              <input
+                type="checkbox"
+                id="certifications"
+                name="helpType"
+                value="certifications"
+                onChange={handleChange}
+                checked={formData.helpType.includes("certifications")}
+              />
               <label className="text-xs lg:text-sm font-thin">
-                CERTIFICATION
+                CERTIFICATIONS
               </label>
             </div>
             <div className="flex gap-1 items-center">
-              <input type="radio" id="" name="" value="" />
+              <input
+                type="checkbox"
+                id="all"
+                name="helpType"
+                value="all"
+                onChange={handleChange}
+                checked={formData.helpType.includes("all")}
+              />
               <label className="text-xs lg:text-sm font-thin">
                 ALL THE ABOVE
               </label>
@@ -56,7 +155,10 @@ const waitlistForm = ({ onNextStep }: { onNextStep: onNextStep }) => {
         </div>
         <button
           onClick={onNextStep}
-          className="text-snow bg-navy p-2 w-full rounded-sm text-center font-bold"
+          className={`p-2 w-full rounded-sm text-center font-bold ${
+            isFormFilled ? "bg-navy text-darkGreeen" : "bg-snow text-navy"
+          }`}
+          // disabled={!isFormFilled}
         >
           FREE TOOLBOX ACCESS
         </button>
